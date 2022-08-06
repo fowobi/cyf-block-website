8b5a18a9b6a108f338767bab787c4c2a5f6ba3c0
fowobi <109865505+fowobi@users.noreply.github.com>
2022-08-06 14:18:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-/* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+/* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */var earnings, input_value, expenses, balance, earning_total, expenses_total;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+earnings = [];
+expenses = [];
+balance = [];
+earning_total = 0;
+expenses_total = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  input_value = getNumberOrString(document.getElementById('input').value);
+  if (input_value > 0) {
+    earnings.unshift(input_value);
+    let element_earnings_list = document.getElementById('earnings-list');
+    let new_li = document.createElement('li');
+    let new_span = document.createElement('span');
+    new_span.innerText = earnings.reduce((a,b) => a+b, 0);
+
+    new_li.appendChild(new_span);
+
+    element_earnings_list.appendChild(new_li);
+  }
+  let element_earn_total = document.getElementById('earn-total');
+  element_earn_total.innerText = earnings.reduce((a,b) => a+b, 0);
+
+});
+
+document.getElementById('button').addEventListener('click', (event) => {
+  input_value = getNumberOrString(document.getElementById('input').value);
+  if (input_value < 0) {
+    expenses.unshift(input_value);
+    let element_expenses_list = document.getElementById('expenses-list');
+    let new_li2 = document.createElement('li');
+    let new_span2 = document.createElement('span');
+    new_span2.innerText = expenses.reduce((a,b) => a+b, 0);
+
+    new_li2.appendChild(new_span2);
+
+    element_expenses_list.appendChild(new_li2);
+  }
+  let element_expense_total = document.getElementById('expense-total');
+  element_expense_total.innerText = expenses.reduce((a,b) => a+b, 0);
+
+});
+
+document.getElementById('button').addEventListener('click', (event) => {
+  balance.unshift(getNumberOrString(document.getElementById('input').value));
+  let element_balance = document.getElementById('balance');
+  element_balance.innerText = balance.reduce((a,b) => a+b, 0);
+
+});
